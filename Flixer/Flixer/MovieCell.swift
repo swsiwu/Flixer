f343f77384f91078be6abe6d92dd3a2e49bb15d0
Si <sw.si.wu@gmail.com>
2018-02-13 03:04:11 -0800

    finished! Now it can refresh and it has poster for every single movie

--- Flixer/Flixer/MovieCell.swift
+++ Flixer/Flixer/MovieCell.swift
@@ -13,6 +13,8 @@ class MovieCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     
+    @IBOutlet weak var posterImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Flixer/Flixer/NowPlayingViewController.swift
+++ Flixer/Flixer/NowPlayingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class NowPlayingViewController:
 UIViewController, UITableViewDataSource {
@@ -14,15 +15,33 @@ UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_refreshControl:)), for: .valueChanged)
+        
+        tableView.insertSubview(refreshControl, at: 0)
+        
+        
         self.tableView.rowHeight = 180
         
         tableView.dataSource = self
+        fetchMovies()
+        
+        
+    }
+    
+    @objc func didPullToRefresh( _refreshControl: UIRefreshControl){
+        
+        fetchMovies()
+    }
+    func fetchMovies() {
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -36,6 +55,7 @@ UIViewController, UITableViewDataSource {
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
                 
                 // TODO: Get the array of movies
                 // TODO: Store the movies in a property to use elsewhere
@@ -44,8 +64,8 @@ UIViewController, UITableViewDataSource {
             }
         }
         task.resume()
-        
     }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
@@ -58,6 +78,13 @@ UIViewController, UITableViewDataSource {
         let overview = movie["overview"] as! String
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterURL)
+        
+        
         
         return cell
     }
