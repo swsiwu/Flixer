f667e7a6997e850434916dc39644b62f9cfab1e8
Si <sw.si.wu@gmail.com>
2018-02-18 00:10:53 -0800

    dd collectionView superhero and details about movies in table cells

--- Flixer/Flixer/ViewControllers/NowPlayingViewController.swift
+++ Flixer/Flixer/ViewControllers/NowPlayingViewController.swift
@@ -90,6 +90,16 @@ UIViewController, UITableViewDataSource {
     }
  
     
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell){
+        let movie = movies[indexPath.row]
+        let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Flixer/Flixer/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flixer
+//
+//  Created by Si Wu on 2/17/18.
+//  Copyright © 2018 Si Wu. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
